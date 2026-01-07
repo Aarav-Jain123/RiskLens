8e46effa79d921daa19bb1a7d47793e8376c08f1
Aarav-Jain123 <137067931+Aarav-Jain123@users.noreply.github.com>
2026-01-07 21:29:53 +0530

    Delete frontend directory

--- frontend/src/main.tsx
+++
@@ -1,7 +0,0 @@
-
-  import { createRoot } from "react-dom/client";
-  import MainApp from "./MainApp.tsx";
-  import "./index.css";
-
-  createRoot(document.getElementById("root")!).render(<MainApp />);
-  
