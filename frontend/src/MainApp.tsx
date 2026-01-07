4f3c1f607234924b8802dd89d3d0ba7c3dff6fe7
Aarav-Jain123 <137067931+Aarav-Jain123@users.noreply.github.com>
2026-01-07 19:17:21 +0530

    Update MainApp.tsx

--- frontend/src/MainApp.tsx
+++ frontend/src/MainApp.tsx
@@ -1,11 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
 import CleanDataSet from './components/CleanDataSet'
 import UnusualDataSet from './components/UnusualDataSet.tsx'
-
+import App from './App.jsx'
 function MainApp() {
   return (
     <Routes>
-      <Route path='/' element={<Home/>}/>
+      <Route path='/' element={<App/>}/>
       <Route path='/clean/' element={<CleanDataSet/>}/>
       <Route path='/unusual/' element={<UnusualDataSet/>}/>
     </Routes>
