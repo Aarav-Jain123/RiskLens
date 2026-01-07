09059a915061bdc2209992ea448de5d57f220b3e
Aarav-Jain123 <137067931+Aarav-Jain123@users.noreply.github.com>
2026-01-07 19:27:17 +0530

    Update MainApp.tsx

--- frontend/src/MainApp.tsx
+++ frontend/src/MainApp.tsx
@@ -1,14 +1,17 @@
+import { BrowserRouter } from "react-router-dom";
 import { Routes, Route } from 'react-router-dom';
 import CleanDataSet from './components/CleanDataSet'
 import UnusualDataSet from './components/UnusualDataSet.tsx'
 import App from './App.jsx'
 function MainApp() {
   return (
-    <Routes>
-      <Route path='/' element={<App/>}/>
-      <Route path='/clean/' element={<CleanDataSet/>}/>
-      <Route path='/unusual/' element={<UnusualDataSet/>}/>
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App/>}/>
+        <Route path='/clean/' element={<CleanDataSet/>}/>
+        <Route path='/unusual/' element={<UnusualDataSet/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 };
 
